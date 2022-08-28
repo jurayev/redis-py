e38bbffac000e85c1d6c90dcb4a925cdd2925d7e
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-08-28 19:26:05 +0200

    feat: Wait for a PING message and send PONGG

--- app/main.py
+++ app/main.py
@@ -3,7 +3,9 @@ import socket
 
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept()
+    sock, addr = server_socket.accept()
+    response = sock.recv(1024)
+    sock.send(b"+PONGG\r\n")
 
 
 if __name__ == "__main__":
